f12dc473396bd9150f65cdda182fa1f0a2f1c0d3
Michael Myung <michael.h.myung@gmail.com>
2018-03-20 17:40:52 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,18 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+    if speed < 60 && speed > 40 
+        false
+    elsif speed > 40
+        true
+    elsif speed < 60
+        true
+    end
+#    speed > 40 || speed < 60 ? true : false
 end
 
-
-
 def not_safe?(speed)
-	
+    speed < 60 && speed > 40 ? false : speed > 40 ? true : speed < 60 ? true : false
 end
 	
 
